60edf187fcb0fb8735901a55dab75f1bbd2d3fba
Yiqiu-Zhang <ezhang13617@gmail.com>
2022-10-19 17:13:02 +0800

    ad mkdssp

--- PDB_File_test.py
+++ PDB_File_test.py
@@ -1,31 +1,95 @@
 
 '''
 /mnt/lustre/zhangyiqiu/rosetta_src_2021.16.61629_bundle/main/source/bin/rosetta_scripts.default.linuxgccrelease
--in:file:s 2hle_remixed.pdb -jd2:delete_old_poses -nstruct 1 -out:chtimestamp -ex1 -ex2 -randomize_missing_coords
--ignore_unrecognized_res -overwrite
--parser:protocol test_peptiderive.xml -scorefile testscore
+-in:file:s 2hle_remixed.pdb -jd2:delete_old_poses -nstruct 1 -out:chtimestamp -ex1 -ex2 -randomize_missing_coords -ignore_unrecognized_res -overwrite -parser:protocol test_peptiderive.xml -scorefile testscore
 
 '''
 
 from Bio.PDB.Chain import Chain
 from Bio.PDB import PDBIO, PDBParser
+from Bio.PDB.DSSP import DSSP
 
-my_chain = Chain("Z")
+MIN_PROT_L = 50
+MAX_PEP_L = 25
+MIN_PEP_L = 5
+
 
 io = PDBIO()
 parser = PDBParser()
 
-structure = parser.get_structure('6fay', '6fay.pdb') # id - string, the id that will be used for the structure
-reslist = []
-for model in structure:
-    model.add(my_chain)
-    for chain in model:
-        for res in chain:
-            res_id = res.id[1]
-            if  900< res_id < 1000:
-                reslist.append(res)
-        for res in reslist:
-            chain.detach_child(res.id)
-            my_chain.add(res)
-io.set_structure(structure)
-io.save('6fay_3.pdb')
+structure = parser.get_structure('6FAY', '6fay.pdb') # id - string, the id that will be used for the structure
+res_list = []
+
+model = structure[0] # Pick the first conformer.
+
+# calculate DSSP, get the secondary structure information.
+# mkdssp -i 1mot.pdb  -o 1mot.dssp
+dssp = DSSP(model, '6fay.dssp', dssp='mkdssp', file_type='DSSP')#
+
+res_index = list(map(lambda x: x[0]-1 if x[3]<0.25 else None,dssp.property_list))
+
+sequence = list(map(lambda x: x[1], dssp.property_list))
+sec_structure = list(map(lambda x: x[2], dssp.property_list))
+rel_acc = list(map(lambda x: x[3], dssp.property_list))
+
+def find_pep(res):
+
+    pep_list = []
+    start = end = i = 0
+    while i < len(res):
+
+        if res[i] is None:  # if i is None, move to the next
+            frag_len = end - start + 1
+            if MIN_PEP_L <= frag_len:
+                pep_list.append((start, end, frag_len))
+
+            start = end = 0
+            i += 1
+
+        else:  # if i is number
+            if not start:
+                start = end = res[i]
+            else:
+                end = res[i]
+            i += 1
+
+    return pep_list
+
+pep_lib = find_pep(res_index)
+
+prot_chain = model.child_list[0]
+prot_len = len(prot_chain)
+
+prot_chain.id = "A"
+pep_chain = Chain("Z")
+third_chain = Chain("B")
+model.add(pep_chain)
+model.add(third_chain)
+
+
+for start, end, frag_len in pep_lib:
+
+    # if frag_len <= MAX_PEP_L: # if fragment length is less than 25, do not apply slide window
+    pep = prot_chain.child_list[start:end+1]  # -1 because peptide label start from 1
+
+    if start < MIN_PROT_L < prot_len-end: # ignore residues before the peptide
+        del_res = prot_chain.child_list[:end+6]  # The residues that will be deleted from the main chain
+
+    elif start > MIN_PROT_L > prot_len - end:
+        del_res = prot_chain.child_list[start-5:]
+
+    elif start > MIN_PROT_L < prot_len - end:
+        del_res = prot_chain.child_list[start-5:]
+        third_chain_res = prot_chain.child_list[end+6:]
+        for res in third_chain_res:
+            third_chain.add(third_chain_res)
+
+    for res in pep:
+        pep_chain.add(res)
+    # noinspection PyUnboundLocalVariable
+    for res in del_res:
+        prot_chain.detach_child(res.id)
+
+    io.set_structure(structure)
+    io.save(f'testfile/6fay_PepID_{start}_{end}.pdb')
+
---
+++ mkdssp.py
@@ -0,0 +1,30 @@
+import os
+import subprocess
+
+#pdb_path = '/home/PJLAB/zhangyiqiu/PycharmProjects/Fragment_data/testfile/6fay_PepID_124_130.pdb'
+#dssp_path = '/home/PJLAB/zhangyiqiu/PycharmProjects/Fragment_data/test.dssp'
+#subprocess.call(['sed', '-i', sed_cmd, pdb_path])
+
+sed_cmd = '1i CRYST1    1.000    1.000    1.000  90.00  90.00  90.00 P 1           1'
+bucket_base = 's3://AF_data/true_structure_dataset/pdb'
+local_base = '/mnt/lustre/zhangyiqiu/Fragment_data/pdb'
+fragment_base = 's3://Fragment_data'
+
+
+
+for i in range(1):
+    pdb_path= f'{bucket_base}/pdb_{i}/'
+    cp_cmd = f'aws s3 cp  . --recursive'
+    subprocess.call(['aws s3', 'cp', f'{bucket_base}/pdb_{i}/',
+                     f'{local_base}/pdb_{i}', '--recursive'])
+    files = os.listdir(f'{local_base}/pdb_{i}')
+    for pdb_file_name in files:
+        pdb_path = f'{local_base}/pdb_{i}/{pdb_file_name}'
+        subprocess.call(['sed', '-i', sed_cmd, pdb_path])
+
+        dssp_name = pdb_file_name.split('.')[0]+'.dssp'
+        dssp_path = f'{local_base}/dssp_{i}/{dssp_name}'
+        subprocess.call(['mkdssp', '-i', pdb_path, '-o', dssp_path])
+
+    subprocess.call(['aws s3', 'cp', f'{local_base}/dssp_{i}',
+                     f'{fragment_base}/dssp/dssp_{i}/', '--recursive'])
