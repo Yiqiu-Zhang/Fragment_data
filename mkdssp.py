b210e2879d29e8f43e04edbd5de2024909bc3225
Yiqiu-Zhang <ezhang13617@gmail.com>
2022-10-19 19:56:46 +0800

    mkdssp

--- mkdssp.py
+++ mkdssp.py
@@ -1,9 +1,6 @@
 import os
 import subprocess
 
-#pdb_path = '/home/PJLAB/zhangyiqiu/PycharmProjects/Fragment_data/testfile/6fay_PepID_124_130.pdb'
-#dssp_path = '/home/PJLAB/zhangyiqiu/PycharmProjects/Fragment_data/test.dssp'
-#subprocess.call(['sed', '-i', sed_cmd, pdb_path])
 
 sed_cmd = '1i CRYST1    1.000    1.000    1.000  90.00  90.00  90.00 P 1           1'
 bucket_base = 's3://AF_data/true_structure_dataset/pdb'
@@ -12,21 +9,25 @@ fragment_base = 's3://Fragment_data'
 
 
 
-for i in range(1):
+for i in range(256):
 
     subprocess.call(['aws', 's3', 'cp', f'{bucket_base}/pdb_{i}/',
                      f'{local_base}/pdb/pdb_{i}', '--recursive'])
-
+    subprocess.call(['mkdir', f'{local_base}/dssp/ds p_{i}'])
     files = os.listdir(f'{local_base}/pdb/pdb_{i}')
+
     for pdb_file_name in files:
 
         pdb_path = f'{local_base}/pdb/pdb_{i}/{pdb_file_name}'
         subprocess.call(['sed', '-i', sed_cmd, pdb_path])
 
-        subprocess.call(['mkdir', f'{local_base}/dssp/dssp_{i}'])
+
         dssp_name = pdb_file_name.split('.')[0]+'.dssp'
         dssp_path = f'{local_base}/dssp/dssp_{i}/{dssp_name}'
         subprocess.call(['mkdssp', '-i', pdb_path, '-o', dssp_path])
 
+
     subprocess.call(['aws', 's3', 'cp', f'{local_base}/dssp/dssp_{i}',
                      f'{fragment_base}/dssp/dssp_{i}/', '--recursive'])
+    subprocess.call(['rm', '-r', f'./pdb/pdb_{i}/*'])
+    subprocess.call(['rm', '-r', f'./dssp/dssp_{i}/*'])
