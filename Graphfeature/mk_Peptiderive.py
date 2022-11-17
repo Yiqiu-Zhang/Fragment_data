3da6fcd4121484245648294f0799f93a2f119ef6
Yiqiu-Zhang <ezhang13617@gmail.com>
2022-11-17 15:19:32 +0800

    multiprocess test

--- Graphfeature/mk_Peptiderive.py
+++ Graphfeature/mk_Peptiderive.py
@@ -1,25 +1,14 @@
-'''
-/mnt/lustre/zhangyiqiu/rosetta_src_2021.16.61629_bundle/main/source/bin/rosetta_scripts.default.linuxgccrelease
--in:file:s 2hle_remixed.pdb -jd2:delete_old_poses -nstruct 1 -out:chtimestamp -ex1 -ex2 -randomize_missing_coords -ignore_unrecognized_res -overwrite -parser:protocol test_peptiderive.xml -scorefile testscore
-
-'''
-
 import os
 import subprocess
 import feature_pipeline as featurepipe
-
+from multiprocessing import Process, Pool
 rosetta_base = '/mnt/lustre/zhangyiqiu/rosetta_src_2021.16.61629_bundle/main/source/bin'
 local_base = '/mnt/lustre/zhangyiqiu/Fragment_data'
 fragment_base = f's3://Fragment_data/frag'
-# rosetta_base = '/home/PJLAB/zhangyiqiu/Documents/rosetta_src_2021.16.61629_bundle/main/source/bin'
-#base = '/home/PJLAB/zhangyiqiu/PycharmProjects/Fragment_data/peptiderive_test/pdb'
-#filename = '1QHP_A_1_renum_179_184.pdb'
-# base_filename = filename.split('.')[0]
 
 mu = -49.48
 std =25.52
 isc_range = [mu-std,mu+std]
-# isc_range = [-1000,0] # test
 
 def parse_peptiderive(peptiderive_str):
 
@@ -41,38 +30,44 @@ def parse_peptiderive(peptiderive_str):
         elif line.startswith('# end chain pair'):
             return descrip
 
-for i in range(1):
-    # subprocess.call(['aws', 's3', 'cp', f'{fragment_base}/frag_{i}', f'{local_base}/frag/frag_{i}', '--recursive'])
-    block = os.listdir(f'{local_base}/frag/frag_{i}')
+def Rosetta(file, i, fragroot):
+    frag_position = file.split('.')[0]
+    subprocess.call(['mkdir','-p', f'{local_base}/pepderive/derive_{i}/{fragroot}/{frag_position}'])
+    subprocess.call([f'{rosetta_base}/rosetta_scripts.default.linuxgccrelease',
+                    '-in:file:s', f'{local_base}/frag/frag_{i}/{fragroot}/{file}',
+                    '-out:path:all', f'{local_base}/pepderive/derive_{i}/{fragroot}/{frag_position}',
+                    '-jd2:delete_old_poses',
+                    '-nstruct 1',
+                    '-out:chtimestamp',
+                    '-ex1',
+                    '-ex2',
+                    '-randomize_missing_coords',
+                    '-ignore_unrecognized_res',
+                    '-overwrite',
+                    '-parser:protocol test_peptiderive.xml'])
+
+    pdbpath = f'{local_base}/pepderive/derive_{i}/{fragroot}/{frag_position}/{frag_position}_0001.pdb'
+
+    with open(f'{local_base}/pepderive/derive_{i}/{fragroot}/{frag_position}/{frag_position}_0001.peptiderive.txt') as f:
+        input_peptiderive_str = f.read()
+        descriptions = parse_peptiderive(input_peptiderive_str)
+
+    featurepipe.PDBtoFeature(descriptions, pdbpath, i, fragroot, frag_position)
+    subprocess.call(['rm', '-r', f'{local_base}/pepderive/derive_{i}/{fragroot}/{frag_position}/'])
+
+for num in range(256):
+    subprocess.call(['aws', 's3', 'cp', f'{fragment_base}/frag_{num}',
+                    f'{local_base}/frag/frag_{num}', '--recursive'])
+    block = os.listdir(f'{local_base}/frag/frag_{num}')
     for fragroot in block:
-        frag_files = os.listdir(f'{local_base}/frag/frag_{i}/{fragroot}')
-        for file in frag_files:
-            frag_position = file.split('.')[0]
-            subprocess.call(['mkdir','-p', f'{local_base}/pepderive/derive_{i}/{fragroot}/{frag_position}'])
-            subprocess.call([f'{rosetta_base}/rosetta_scripts.default.linuxgccrelease',
-                            '-in:file:s', f'{local_base}/frag/frag_{i}/{fragroot}/{file}',
-                            '-out:path:all', f'{local_base}/pepderive/derive_{i}/{fragroot}/{frag_position}',
-                            '-jd2:delete_old_poses',
-                            '-nstruct 1',
-                            '-out:chtimestamp',
-                            '-ex1',
-                            '-ex2',
-                            '-randomize_missing_coords',
-                            '-ignore_unrecognized_res',
-                            '-overwrite',
-                            '-parser:protocol test_peptiderive.xml'])
+        frag_files = os.listdir(f'{local_base}/frag/frag_{num}/{fragroot}')
+        pool = Pool(128)
 
-            pdbpath = f'{local_base}/pepderive/derive_{i}/{fragroot}/{frag_position}/{frag_position}_0001.pdb'
+        for pdb_file in frag_files:
+            pool.apply_async(func=Rosetta, args=(pdb_file, num, fragroot,))
 
-            with open(f'{local_base}/pepderive/derive_{i}/{fragroot}/{frag_position}/{frag_position}_0001.peptiderive.txt') as f:
-                input_peptiderive_str = f.read()
-                descriptions = parse_peptiderive(input_peptiderive_str)
+        pool.close()
+        pool.join()
 
-            featurepipe.PDBtoFeature(descriptions, pdbpath, i, fragroot, frag_position)
-            subprocess.call(['rm', '-r', f'{local_base}/pepderive/derive_{i}/{fragroot}/{frag_position}/'])
-        subprocess.call(['rm', '-r', f'{local_base}/frag/frag_{i}/{fragroot}/'])
+        subprocess.call(['rm', '-r', f'{local_base}/frag/frag_{num}/{fragroot}/'])
 
-            # keep receprot in the pdb file,
-            # remove the third chain
-            # remove the unused peptide area
-            # put the receptor and the peptide in the same pdb
