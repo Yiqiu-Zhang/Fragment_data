4d4e196a9ec181b0706e6390049bde7416c98f30
Yiqiu-Zhang <ezhang13617@gmail.com>
2022-11-21 15:49:36 +0800

    multiprocess test

--- Graphfeature/test.py
+++ Graphfeature/test.py
@@ -3,4 +3,4 @@ local_base = '/mnt/lustre/zhangyiqiu/Fragment_data'
 for num in range(1,50):
     subprocess.call(['aws', 's3', 'cp', f's3://Fragment_data/frag/frag_4',
                      f'/mnt/lustre/zhangyiqiu/Fragment_data/frag/frag_4','--recursive'],
-                    stdout=subprocess.DEVNULL,)
+                    stdout=subprocess.DEVNULL)
