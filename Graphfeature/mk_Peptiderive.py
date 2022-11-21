d0de457d797cf1d4ab0c540042c857a91fef7712
Yiqiu-Zhang <ezhang13617@gmail.com>
2022-11-21 18:02:14 +0800

    multiprocess test

--- Graphfeature/mk_Peptiderive.py
+++ Graphfeature/mk_Peptiderive.py
@@ -71,8 +71,7 @@ for num in range(1,30): # 0,256
         pool.join()
 
     subprocess.call(['aws', 's3', 'cp', f'{local_base}/feature/feat_{num}/',
-                    f's3://Fragment_data/feature/feat_{num}', '--recursive'],
-                    stdout=subprocess.DEVNULL)
+                    f's3://Fragment_data/feature/feat_{num}', '--recursive'])
     subprocess.call(['rm', '-r', f'{local_base}/feature/feat_{num}/'])
     subprocess.call(['rm', '-r', f'{local_base}/pepderive/derive_{num}/'])
     subprocess.call(['rm', '-r', f'{local_base}/frag/frag_{num}/'])
