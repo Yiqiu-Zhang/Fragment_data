756974227ef4dc6a7b5bd26156ecbe87a9bebed2
Yiqiu-Zhang <ezhang13617@gmail.com>
2022-11-21 16:45:19 +0800

    multiprocess test

--- Graphfeature/test.py
+++ Graphfeature/test.py
@@ -3,8 +3,7 @@ import os
 # export https_proxy="http://10.1.8.5:32680"
 local_base = '/mnt/lustre/zhangyiqiu/Fragment_data'
 
-os.system('aws s3 cp s3://Fragment_data/frag/frag_4 '
-          '/mnt/lustre/zhangyiqiu/Fragment_data/frag/frag_4 --recursive >/dev/null')
+os.system('aws s3 cp s3://Fragment_data/frag/frag_4 /mnt/lustre/zhangyiqiu/Fragment_data/frag/frag_4 --recursive > /dev/null')
 '''
 subprocess.call(['aws', 's3', 'cp', f's3://Fragment_data/frag/frag_4',
                      f'/mnt/lustre/zhangyiqiu/Fragment_data/frag/frag_4','--recursive', '>/dev/null'],
