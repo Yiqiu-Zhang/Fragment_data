dfc3c6631ce0c7750732a09183f0590ed42b58ce
Yiqiu-Zhang <ezhang13617@gmail.com>
2022-11-21 17:43:50 +0800

    multiprocess test

--- Graphfeature/test.py
+++ Graphfeature/test.py
@@ -4,10 +4,7 @@ import os
 local_base = '/mnt/lustre/zhangyiqiu/Fragment_data'
 '''
 os.system('aws s3 cp s3://Fragment_data/frag/frag_4 /mnt/lustre/zhangyiqiu/Fragment_data/frag/frag_5 --recursive > /dev/null')
-
-subprocess.call(['aws', 's3', 'cp', f's3://Fragment_data/frag/frag_4',
-                f'/mnt/lustre/zhangyiqiu/Fragment_data/frag/frag_4','--recursive', '> /dev/null'],
-                shell=True)#
 '''
-subprocess.call('aws s3 cp s3://Fragment_data/frag/frag_4 /mnt/lustre/zhangyiqiu/Fragment_data/frag/frag_5 --recursive > /dev/null',
-                shell=True)
+subprocess.call(['aws', 's3', 'cp', f's3://Fragment_data/frag/frag_4',
+                f'/mnt/lustre/zhangyiqiu/Fragment_data/frag/frag_4','--recursive'],
+                )
